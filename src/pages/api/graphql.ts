a3e3d5b79a085b1f961cc3e2d1cef1d61eb6e52e
Juan Carlos <jcxkdev@gmail.com>
2020-11-16 11:54:06 +0100

    add cors specifics

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,17 +1,23 @@
-import { ApolloServer, gql } from 'apollo-server-micro';
-import microCors from 'micro-cors';
+import { ApolloServer } from 'apollo-server-micro';
+import Cors from 'micro-cors';
 import { resolversObj } from '@/api/resolvers';
 import { join } from 'path';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { addResolversToSchema } from '@graphql-tools/schema';
 import dataSources from '@/api/dataSources/';
-console.log(process.cwd(), __dirname);
+
 const schema = loadSchemaSync(
   join(process.cwd(), '/src/api/schema/*.graphql'),
   { loaders: [new GraphQLFileLoader()] }
 );
+
 //const { RedisCache } = require('apollo-server-cache-redis');
+
+const cors = Cors({
+  allowMethods: ['POST', 'OPTIONS'],
+});
+
 export const config = {
   api: {
     bodyParser: false,
@@ -22,7 +28,11 @@ const conf = {
   dataSources,
   introspection: true,
   playground: true,
+  context(ctx) {
+    return ctx;
+  },
   schema: addResolversToSchema(schema, resolversObj),
+
   /*
   cache: new RedisCache({
       host: 'localhost',
@@ -33,9 +43,7 @@ const conf = {
   },
   */
 };
-const cors = microCors();
-const apolloServer = new ApolloServer(conf);
-const handler = apolloServer.createHandler({ path: '/api/graphql' });
-export default cors((req, res) =>
-  req.method === 'OPTIONS' ? res.end() : handler(req, res)
-);
+
+const server = new ApolloServer(conf);
+const handler = server.createHandler({ path: '/api/graphql' });
+export default cors(handler);
